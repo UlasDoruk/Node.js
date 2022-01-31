36ddd772b63aeb5c4df97eb09bfc16994577add5
UlaşDK <udk97@hotmail.com>
2022-01-31 16:23:16 +0300

    Node.js Daire Alanı

---
+++ index_1.js
@@ -0,0 +1,13 @@
+//let arguments  = process.argv.slice(1)
+
+let n = 5
+
+function area(radius){
+    var formulOfArea = Math.PI*(radius**2)
+    formulOfArea =  formulOfArea.toFixed(1)
+    return console.log(`Radius : ${radius} Area of Circle : ${formulOfArea}`)
+}
+
+area(n)
+
+//area(arguments*1)
